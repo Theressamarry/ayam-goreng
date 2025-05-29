c3420ea4561baf383cd5ac2e73b00077385853ef
THERESSA MARRY CHRISTIANTY <thech160304@gmail.com>
2025-05-29 19:29:01 +0700

    update fitur penjualan pada kasir

--- Kasir.cpp
+++ Kasir.cpp
@@ -1,11 +1,28 @@
 #include "Kasir.h"
+#include "ProdukTerjual.h"
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<ProdukTerjual> daftarPenjualan; // menyimpan data penjualan
 
 Kasir::Kasir(int id, string uname, string pwd, string idKaryawan)
     : User(id, uname, pwd, "Kasir"), idKaryawan(idKaryawan){}
 
 void Kasir::kelolaPenjualan(){
-    cout << "Kasir mencatat penjualan" << endl;
+    int id, jumlah;
+    string tgl, nama;
+    double harga;
+
+    cout << "=== C A T A T  P E N J U A L A N ===" << endl;
+    cout << "ID Penjualan: "; cin >> id;
+    cout << "Tanggal (DD-MM-YYYY): "; cin >> tgl;
+    cout << "Nama Produk: "; cin >> nama;
+    cout << "Jumlah: "; cin >> jumlah;
+    cout << "Harga per unit: "; cin >> harga;
+    daftarPenjualan.push_back(ProdukTerjual(id, tgl, nama, jumlah, harga));
+    cout << "Penjualan berhasil dicatat!" << endl;
 }
 
 void Kasir::lihatStok(){
